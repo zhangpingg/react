28ae30f483ebea696cabf1745dbd0ece80d9841a
zhangping <2425238116@qq.com>
2025-04-17 23:19:05 +0800

    feat: 手写表格滚动

--- vite-react-h5/src/pages/test/const.js
+++ vite-react-h5/src/pages/test/const.js
@@ -1,11 +1,20 @@
-const header = [{ name: '品牌1' }, { name: '品牌2' }, { name: '品牌3' }, { name: '品牌4' }];
-
-const data = [
-    { name: '厂家名称', aa: '名称1', bb: '名称2', cc: '名称3', dd: '名称4' },
-    { name: '规格', aa: '规格1', bb: '规格2', cc: '规格3', dd: '规格4' },
-    { name: '层级', aa: '层级1', bb: '层级2', cc: '层级3', dd: '层级4' },
-    { name: '花纹', aa: '花纹1', bb: '花纹2', cc: '花纹3', dd: '花纹4' },
-    { name: '车型', aa: '车型1', bb: '车型2', cc: '车型3', dd: '车型4' },
+const resHeader = [
+    { id: 0, name: '' },
+    { id: 1, name: '品牌1' },
+    { id: 2, name: '品牌2' },
+    { id: 3, name: '品牌3' },
+    { id: 4, name: '品牌4' },
+    { id: 5, name: '品牌5' },
 ];
 
-export { header, data };
+const resData = [
+    { id: 0, aa: '厂家名称', bb: '规格', cc: '层级', dd: '花纹', ee: '车型' },
+    { id: 1, aa: '名称1', bb: '规格1', cc: '层级1', dd: '花纹1', ee: '车型1' },
+    { id: 2, aa: '名称2', bb: '规格2', cc: '层级2', dd: '花纹2', ee: '车型2' },
+    { id: 3, aa: '名称3', bb: '规格3', cc: '层级3', dd: '花纹3', ee: '车型3' },
+    { id: 4, aa: '名称4', bb: '规格4', cc: '层级4', dd: '花纹4', ee: '车型4' },
+    { id: 5, aa: '名称5', bb: '规格5', cc: '层级5', dd: '花纹5', ee: '车型5' },
+];
+
+export { resHeader, resData };
+
--- vite-react-h5/src/pages/test/index.jsx
+++ vite-react-h5/src/pages/test/index.jsx
@@ -1,21 +1,63 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import cn from 'classnames';
+import _ from 'lodash';
 import './styles.less';
-import { header, data } from './const.js';
+import { resHeader, resData } from './const.js';
 
 const Index = () => {
+    const [fixedRowId, setFixedRowId] = useState();
+    const [header, setHeader] = useState(resHeader);
+    const [list, setList] = useState(resData);
+
+    // 获取索引
+    const getIndex = (list, id) => {
+        let _index = 0;
+        list.map((item, index) => {
+            if (item.id === id) {
+                _index = index;
+            }
+        });
+        return _index;
+    };
+    // 固定列
+    const fixedRow = (item) => {
+        if (item.id === fixedRowId) {
+            setFixedRowId(null);
+        } else {
+            setFixedRowId(item.id);
+            setHeader((prev) => {
+                console.log(11, prev);
+                let _list = _.cloneDeep(prev);
+                var _removedItem = _list.splice(getIndex(_list, item.id), 1)[0];
+                _list.splice(1, 0, _removedItem);
+                console.log(22, _list);
+                return _list;
+            });
+            setList((prev) => {
+                let _list = _.cloneDeep(prev);
+                var _removedItem = _list.splice(getIndex(_list, item.id), 1)[0];
+                _list.splice(1, 0, _removedItem);
+                return _list;
+            });
+        }
+    };
+
     return (
         <div className="box">
             <div className="table-container">
                 <div className="table-header">
-                    <p className="table-header-cell table-header-cellSticky fs-14"></p>
                     {header.map((item, index) => {
                         return (
                             <p
                                 key={index}
                                 className={cn({
                                     'table-header-cell fs-14': true,
+                                    'table-header-cellSticky': index === 0,
+                                    'table-header-cellSticky2': fixedRowId === item.id,
                                 })}
+                                onClick={() => {
+                                    fixedRow(item);
+                                }}
                             >
                                 {item.name}
                             </p>
@@ -23,14 +65,21 @@ const Index = () => {
                     })}
                 </div>
                 <div className="table-body">
-                    {data.map((item, index) => {
+                    {list.map((item, index) => {
                         return (
-                            <div key={index} className="table-body-row">
-                                <p className="table-body-cell table-body-cellSticky fs-14">{item.name}</p>
-                                <p className="table-body-cell fs-14 ">{item.aa}</p>
-                                <p className="table-body-cell fs-14">{item.bb}</p>
+                            <div
+                                key={index}
+                                className={cn({
+                                    'table-body-row': true,
+                                    'table-body-row-cellSticky': index === 0,
+                                    'table-body-row-cellSticky2': item.id === fixedRowId,
+                                })}
+                            >
+                                <p className="table-body-cell fs-14">{item.aa}</p>
+                                <p className="table-body-cell fs-14 ">{item.bb}</p>
                                 <p className="table-body-cell fs-14">{item.cc}</p>
                                 <p className="table-body-cell fs-14">{item.dd}</p>
+                                <p className="table-body-cell fs-14">{item.ee}</p>
                             </div>
                         );
                     })}
@@ -41,3 +90,4 @@ const Index = () => {
 };
 
 export default Index;
+
