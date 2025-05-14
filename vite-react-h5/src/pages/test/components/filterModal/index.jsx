36fdac7b321e71303747ccc4501c0cff0c4e8762
zhangping <2425238116@qq.com>
2025-05-14 18:00:17 +0800

    test: 滚动

---
+++ vite-react-h5/src/pages/test/components/filterModal/index.jsx
@@ -0,0 +1,13 @@
+/*
+ * @Author: zhangping
+ * @Date: 2025-05-14 17:56:55
+ * @Description: 弹框内滚动
+ */
+
+import React, { useEffect } from 'react';
+
+const Index = () => {
+    return <div>home</div>;
+};
+
+export default Index;
--- vite-react-h5/src/pages/test/index.jsx
+++ vite-react-h5/src/pages/test/index.jsx
@@ -4,10 +4,68 @@
  * @Description: 菜单名称-模块名称
  */
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
+import BScroll from 'better-scroll';
+import { Button, Popup } from 'antd-mobile';
+import './styles.less';
 
 const Index = () => {
-    return <div>test</div>;
+    const provinceColumnRef = useRef(null);
+    // 存储 better-scroll 实例
+    const scrollInstances = useRef({
+        province: null,
+        city: null,
+        highway: null,
+        serviceArea: null,
+    });
+
+    // 初始化 better-scroll 实例
+    const initScroll = (ref, columnName) => {
+        if (ref.current && !scrollInstances.current[columnName]) {
+            scrollInstances.current[columnName] = new BScroll(ref.current, {
+                scrollY: true,
+                click: true,
+                bounce: {
+                    top: false,
+                    bottom: false,
+                },
+            });
+        }
+    };
+    // 获取数据
+    const getList = () => {};
+
+    useEffect(() => {
+        initScroll(provinceColumnRef, 'province');
+        return () => {
+            Object.keys(scrollInstances.current).forEach((key) => {
+                destroyScroll(key);
+            });
+        };
+    }, []);
+
+    return (
+        <div ref={provinceColumnRef} className="box">
+            <div>
+                <div>内容</div>
+                <div>内容</div>
+                <div>内容</div>
+                <div>内容</div>
+                <div>内容</div>
+                <div>内容</div>
+                <div>内容</div>
+                <div>内容</div>
+                <div>内容</div>
+                <div>内容</div>
+                <div>内容</div>
+                <div>内容</div>
+                <div>内容</div>
+                <div>内容</div>
+                <div>内容</div>
+                <div>内容</div>
+            </div>
+        </div>
+    );
 };
 
 export default Index;
