d1a3851d3d9a6b73cb30481e20f4a63596184991
zhangping <2425238116@qq.com>
2025-07-14 10:37:47 +0800

    feat: 分词高亮

--- vite-react-h5/src/pages/highlightText/index.jsx
+++ vite-react-h5/src/pages/highlightText/index.jsx
@@ -9,18 +9,28 @@ import React from 'react';
 const Index = () => {
     let str = '1234abcd1234ABCD';
 
-    // 高亮文本
-    const highlightText = (str, keyword) => {
+    // 整体-高亮文本
+    const wholeHighlightText = (str, keyword) => {
         const regex = new RegExp(keyword, 'ig');
         const highlightedText = str.replace(regex, (match) => {
             return `<span style='color:#f00'>${match}</span>`;
         });
         return highlightedText;
     };
+    // 分词-高亮文本
+    const participleHighlightText = (str, keyword) => {
+        const uniqueChars = [...new Set(keyword.split(''))]; // 去重
+        const pattern = uniqueChars.map((char) => `(${char})`).join('|');
+        const regex = new RegExp(pattern, 'gi');
+        return str.replace(regex, (match) => {
+            return `<span style='color:#f00'>${match}</span>`;
+        });
+    };
 
     return (
         <div>
-            <span dangerouslySetInnerHTML={{ __html: highlightText(str, 'ab') }}></span>
+            <div dangerouslySetInnerHTML={{ __html: wholeHighlightText(str, 'ab') }}></div>
+            <div dangerouslySetInnerHTML={{ __html: participleHighlightText(str, '14') }}></div>
         </div>
     );
 };
