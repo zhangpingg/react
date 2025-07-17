cea23175ad0b386568e509a899d312f07386a7f8
zhangping <2425238116@qq.com>
2025-07-17 15:05:27 +0800

    feat：分词高亮

--- vite-react-h5/src/pages/highlightText/index.jsx
+++ vite-react-h5/src/pages/highlightText/index.jsx
@@ -9,7 +9,7 @@ import React from 'react';
 const Index = () => {
     let str = '1234abcd1234ABCD';
 
-    // 整体-高亮文本
+    // 整体匹配-高亮文本
     const wholeHighlightText = (str, keyword) => {
         const regex = new RegExp(keyword, 'ig');
         const highlightedText = str.replace(regex, (match) => {
@@ -17,7 +17,7 @@ const Index = () => {
         });
         return highlightedText;
     };
-    // 分词-高亮文本
+    // 分词匹配（乱序）-高亮文本
     const participleHighlightText = (str, keyword) => {
         const uniqueChars = [...new Set(keyword.split(''))]; // 去重
         const pattern = uniqueChars.map((char) => `(${char})`).join('|');
@@ -26,11 +26,33 @@ const Index = () => {
             return `<span style='color:#f00'>${match}</span>`;
         });
     };
+    // 分词匹配（在字符串中匹配的时候，字符串中间可以有任意别的内容，且顺序一致）-高亮文本
+    const participleSameOrderHighlightText = (str, keyword) => {
+        if (!keyword || !str) return str;
+        let result = '';
+        let i = 0; // 指向 keyword 的指针
+        const lowerKeyword = keyword.toLowerCase();
+        const lowerStr = str.toLowerCase();
+        for (let idx = 0; idx < str.length; idx++) {
+            const char = str[idx];
+            const lowerChar = lowerStr[idx];
+
+            if (i < lowerKeyword.length && lowerChar === lowerKeyword[i]) {
+                result += `<span style="color: #f00;">${char}</span>`;
+                i++;
+            } else {
+                result += char;
+            }
+        }
+
+        return i === keyword.length ? result : str;
+    };
 
     return (
         <div>
             <div dangerouslySetInnerHTML={{ __html: wholeHighlightText(str, 'ab') }}></div>
             <div dangerouslySetInnerHTML={{ __html: participleHighlightText(str, '14') }}></div>
+            <div dangerouslySetInnerHTML={{ __html: participleSameOrderHighlightText(str, '14da') }}></div>
         </div>
     );
 };
